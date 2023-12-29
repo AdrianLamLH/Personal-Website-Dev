710d1ec1b3107527969dc947f7791cab7881b013
AdrianLamLH <adrianlam@g.ucla.edu>
2023-12-28 23:29:41 -0800

    Search Filtering works (inverted)
    Searches correctly filter through list of terms, but starts off showing whole list rather than after submitting form.

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -5,23 +5,24 @@ import { useState, useRef } from 'react';
 function SearchBar () {
     // const SearchBox = () => {}
     const [query, setQuery] = useState("");
-    const [terms, setTerms] = useState([])
+    const [terms, setTerms] = useState(["hey", "hello" ,"blaeh", "boooboo", "yeay","hooray"])
     const inputRef = useRef();
     const foundTerms = terms.filter((term) => {return term.includes(query)});
 
     function checkTerm (e) {
-        const terms = ["hey", "hello" ,"blaeh", "boooboo", "yeay","hooray"];
-        // alert('it works!');
-        setTerms(["hey", "hello" ,"blaeh", "boooboo", "yeay","hooray"]);
         e.preventDefault();
-        alert(inputRef.current.value)
-        // return (
-        // )
+        // alert(inputRef.current.value);
+        if (inputRef.current.value === "") return;
+        setTerms(prev => {
+            return [...prev,inputRef.current.value]
+        });
+        alert(foundTerms)
+        inputRef.current.value = "";
     };
 
     return (
         <>
-        {terms.map(term  => <ul>{term}</ul>)}
+        {foundTerms.map(term => <ul>{term}</ul>)}
         <form action="" onSubmit={checkTerm}>
             <div className="search-form">
                 <input className="search-box" name="query" placeholder="Message Adrian's AI..."  value={query} ref={inputRef} onChange={(e) => setQuery(e.target.value)}/>
